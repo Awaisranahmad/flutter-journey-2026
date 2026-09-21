669b9008ff773cf7d4af68fedffd7f0de7cb520f
Rana Awais Ahmad <rawaisah124@gmail.com>
2026-09-21 21:52:43 +0500

    learning of expand and reterive data from product model

--- day-06-provider-state/flutter_application_1/lib/widgets/products_card.dart
+++ day-06-provider-state/flutter_application_1/lib/widgets/products_card.dart
@@ -11,7 +11,7 @@ class ProductCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      elevation: 2,
+      elevation: 4,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: Padding(
         padding: const EdgeInsets.all(12),
